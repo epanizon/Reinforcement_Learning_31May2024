ca66aadbc5ccc1136dc26438604131cadab376f7
epanizon <32537955+epanizon@users.noreply.github.com>
2024-05-22 15:59:22 +0200

    Update main_classes.py
    Just removed spaces

--- DDPG/main_classes.py
+++ DDPG/main_classes.py
@@ -1,4 +1,4 @@
-import torch 
+deimport torch 
 import torch.nn as nn 
 import torch.nn.functional as F 
 import torch.autograd 
@@ -61,41 +61,30 @@ class Replay_Memory:
     def __init__(self, max_size): 
         
         self.max_size = max_size 
-        
         self.buffer = deque(maxlen = max_size)
         
     def push(self, state, action, reward, next_state, done): 
         
         experience = (state, action, np.array([reward]), next_state, done)
-        
         self.buffer.append(experience)
         
     def sample(self, batch_size):
         
         state_batch = []
-        
         action_batch = [] 
-        
         reward_batch = [] 
-        
         next_state_batch = [] 
-        
         done_batch = [] 
-        
         batch = random.sample(self.buffer, batch_size)
         
         for experience in batch: 
             
             state, action, reward, next_state, done = experience 
-            
-            state_batch.append(state)
-            
+    
+            state_batch.append(state)        
             action_batch.append(action) 
-            
             reward_batch.append(reward) 
-            
             next_state_batch.append(next_state)
-            
             done_batch.append(not done)
             
         return state_batch, action_batch, reward_batch, next_state_batch, done_batch 
