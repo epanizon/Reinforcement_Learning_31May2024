ffcb1df841eb99727387e693c3d3cd0e028cd98d
epanizon <32537955+epanizon@users.noreply.github.com>
2024-05-23 11:10:51 +0200

    Update main_classes.py
    Now DDPGAgent is consistent with new changes to adjustable size/depths of actors/critics.
    Added comments throughout

--- DDPG/main_classes.py
+++ DDPG/main_classes.py
@@ -231,103 +231,111 @@ class Actor(nn.Module):
         
         
         
-        
 class DDPGagent: 
-    
-    def __init__(self, env, layer1_size, layer2_size,replay_min=100,replay_size=1000000,critic_lr=0.00015, actor_lr=0.000015, tau =0.001, gamma=0.99,loss =nn.MSELoss(), batch_size=64, name_critic = None, name_actor=None, device = "cpu" ,directory = "models"):
+    """
+    Initializes an agent which uses the Deep Deterministic Policy Gradient agent 
+    """
+    def __init__(self, 
+                 env, 
+                 hidden_layers_dims,
+                 replay_min=100,
+                 replay_size=1000000,
+                 critic_lr=0.00015, 
+                 actor_lr=0.000015, 
+                 tau =0.001, 
+                 gamma=0.99,
+                 loss=nn.MSELoss(), 
+                 batch_size=64, 
+                 name_critic=None, 
+                 name_actor=None, 
+                 device = "cpu",
+                 directory = "models"):
         
+        # "reads" the environment and sets
         self.env = env 
-                       
-        
         self.input_dim = env.observation_space.shape 
-        
         self.n_actions = env.action_space.shape[0]
-        
         self.tau = tau 
-        
         self.device = device
-        
         self.gamma = gamma 
         
+        # 
         self.batch_size = batch_size 
-        
-        self.name_critic = name_critic
-                                      
-        
         self.memory= Replay_Memory(replay_size)
-        
         self.replay_min = replay_min
         
+        # sets the names used for folder creation and checkpoints saves
+        self.name_critic = name_critic
         self.name_actor = name_actor 
         
-        self.critic = Critic(self.input_dim, layer1_size, layer2_size, self.n_actions, name = name_critic,chkpt=directory).to(device)
-        
+        # creates critic
+        self.critic = Critic(self.input_dim, hidden_layers_dims, self.n_actions, name=name_critic, chkpt=directory).to(device)
         name_target_critic = None
         
         if name_critic is not None: 
-            
             name_target_critic = name_critic + "_target"
-                
-        self.target_critic = Critic(self.input_dim, layer1_size, layer2_size, self.n_actions, name = name_target_critic,chkpt=directory).to(device)
         
+        # creates target critic
+        self.target_critic = Critic(self.input_dim, hidden_layers_dims, self.n_actions, self.n_actions, name = name_target_critic,chkpt=directory).to(device)
+        
+        
+        # creates actor
         self.actor = Actor(self.input_dim, layer1_size, layer2_size, self.n_actions, name = name_actor,chkpt=directory).to(device)
         
         name_target_actor = None 
-        
         if name_actor is not None: 
+                name_target_actor = name_actor + "_target"
             
-            name_target_actor = name_actor + "_target"
-            
+        # creates target actor
         self.target_actor = Actor(self.input_dim, layer1_size, layer2_size, self.n_actions, name = name_target_actor,chkpt=directory).to(device)
         
+        # initialization of weights (possibly redundant)
         self.critic.init_weights()
-        
         self.actor.init_weights()
-        
+
+        # initialization of weights (possibly redundant)
         self.update_target_weights()
         
         self.critic_criterion = loss 
-        
         self.actor_criterion = loss  
         
+        # initialization of optimizers
         self.critic_optimizer = torch.optim.Adam(self.critic.parameters(),lr=critic_lr,weight_decay=0.01)
-        
         self.actor_optimizer = torch.optim.Adam(self.actor.parameters(),lr=actor_lr)
     
-    def update_critic_optimizer(self, learning_rate): 
-        
+    # explict decay of learning rates
+    def update_critic_optimizer(self, learning_rate):
         self.critic_optimizer = torch.optim.Adam(self.critic.parameters(),lr=learning_rate)
         
     def update_actor_optimizer(self, learning_rate): 
-        
         self.actor_optimizer = torch.optim.Adam(self.actor.parameters(),lr=learning_rate)
         
-        
+    
     def update_replay_memory(self,state, action, reward, next_state, done): 
-        
+        """ Adds single experience to the memory buffer.
+        Receives s,a,r,s',done
+        """
         self.memory.push(state, action, reward, next_state, done)    
-        
-        
+
         
     def update_target_weights(self,tau=1): 
+        """ Soft-update of the target networks towards the current (learned) network.
+        """
         
         for target_param, param in zip(self.target_critic.parameters(),self.critic.parameters()): 
-            
             target_param.data.copy_(param.data * self.tau + target_param.data *(1.0 - tau))
             
         for target_param, param in zip(self.target_actor.parameters(),self.actor.parameters()): 
-            
             target_param.data.copy_(param.data * self.tau + target_param.data *(1.0 - tau))
             
             
     def get_action(self, observation): 
-        
+        """ From state (observation) to the deterministic (+noise) action
+        """
         self.actor.eval()  #because I have batch norm 
         
         observation = torch.tensor(observation, dtype= torch.float).to(self.device)
-     
         actor_action = self.actor(observation)
-        
         action = actor_action.cpu().detach().numpy()  
         
         return action 
@@ -335,120 +343,72 @@ class DDPGagent:
     
     def train(self): 
         
+        # training starts only after some sampling has been done
         if len(self.memory) <  self.replay_min:
-            
             return 
         
+        # randomly sampled experience from past.
         states, actions, rewards, next_states, not_done = self.memory.sample(self.batch_size)
         
         states = torch.tensor(np.array(states), dtype = torch.float).to(self.device)
-        
         actions = torch.tensor(np.array(actions), dtype = torch.float).to(self.device)
-        
         rewards = torch.tensor(np.array(rewards), dtype = torch.float).to(self.device)
-        
         next_states = torch.tensor(np.array(next_states), dtype = torch.float).to(self.device)
-        
         not_done = torch.tensor(np.array(not_done)).unsqueeze(1).to(self.device)
         
-        self.actor.eval() 
-        
+        # 
+        self.actor.eval()     
         self.critic.eval() 
         
+
         self.target_actor.eval() 
-        
         self.target_critic.eval() 
         
+        # a' = mu(s')
+        # target q -> q(s', mu)
         target_actions = self.target_actor.forward(next_states)
-        
         target_critic_value = self.target_critic(next_states, target_actions) 
-        
     
-        
+        # Q^exp = r + gamma Q(s',a')
         targets = rewards + self.gamma*not_done*target_critic_value
-        targets.to(self.device) 
-        
-        
-       
-        
-         
+        targets.to(self.device)          
         
         self.critic.train()
-        
         self.critic_optimizer.zero_grad()
-        
         critic_value = self.critic.forward(states, actions)
         
-      
-        
+        # if MSE, loss = (r + gamma Q(s', a') - Q(s,a))^2
         loss = self.critic_criterion(critic_value, targets)
-        
         loss.backward() 
         
         self.critic_optimizer.step() 
-        
         self.critic.eval() 
         
         self.actor_optimizer.zero_grad() 
-        
         self.actor.train() 
         
         mu = self.actor.forward(states)
         
+        # if MSE, loss =  - Q(s, mu(s))^2 
+        #     --> grad ~ -Q(s,mu) grad mu(s)
         actor_loss = -self.critic.forward(states,mu)
-        
         actor_loss = torch.mean(actor_loss)
-        
         actor_loss.backward()
-        
         self.actor_optimizer.step()
-        
         self.update_target_weights(self.tau)
         
     def save_model(self):
-        
+        """ Saves all models' checkpoints in folder given by names
+        """
         self.actor.save_checkpoint()
-        
         self.critic.save_checkpoint()
-        
         self.target_actor.save_checkpoint()
-        
         self.target_critic.save_checkpoint()
 
     def load_model(self):
-        
+        """ Loads all models' checkpoints in folder given by names
+        """
         self.actor.load_checkpoint()
-        
         self.critic.load_checkpoint()
-        
         self.target_actor.load_checkpoint()
-        
         self.target_critic.load_checkpoint()
-        
-        
-        
-        
-        
-        
-        
-
-
-        
-                   
-        
-        
-        
-        
-        
-
-
-        
-        
-       
-
-
-               
-
-
-
-
